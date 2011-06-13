ec1e2888962ecc1a7d70d7a0692f82386fd6a028
webpat <patrick.merheb@gmail.com>
2011-06-13 20:04:15 +1100

    Manager

---
+++ src/main/java/org/jquant/manager/InstrumentManager.java
@@ -0,0 +1,16 @@
+package org.jquant.manager;
+
+import org.jquant.core.Symbol;
+import org.jquant.serie.CandleSerie;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstrumentManager {
+
+	
+	public CandleSerie getCandleSerie(Symbol symbol){
+		return null;
+		
+	}
+	
+}
---
+++ src/test/java/com/sgam/jquant/MarketManagerTest.java
@@ -0,0 +1,37 @@
+package com.sgam.jquant;
+
+
+import org.easymock.EasyMock;
+import org.jquant.data.DataProvider;
+import org.jquant.data.ProviderAdapterFactory;
+import org.jquant.data.reader.ICandleReader;
+import org.jquant.manager.MarketManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.unitils.easymock.annotation.Mock;
+import org.unitils.inject.annotation.TestedObject;
+import org.unitils.spring.annotation.SpringApplicationContext;
+
+@SpringApplicationContext(value="jquant-test.xml")
+public class MarketManagerTest {
+
+	@Mock
+	private ICandleReader candleReader;
+	
+	private ProviderAdapterFactory readerFactory;
+	
+	@TestedObject
+	private MarketManager marketMgr;
+	
+	@Before
+	public void setUp() throws Exception {
+		EasyMock.replay(this);
+		EasyMock.expect(readerFactory.getCandleReader(DataProvider.BLOOMBERG)).andReturn(candleReader);
+	}
+	
+	@Test
+	public void testReadCandle(){
+		
+	}
+
+}
